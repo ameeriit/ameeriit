79431ac533d1f7f89d8f492e2b71a37bd9a349a8
@meeriit <amritpokharel51@gmail.com>
2023-06-07 09:26:38 +0545

    add h1 text background image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,29 @@
 import bannerpic from "../../assets/images/banner-pic.png";
 
 const Home = () => {
+  const rainbowStyle = {
+    backgroundImage:
+      "linear-gradient(-90deg, #adfbda 0, #35c3ff 30%, #fda399 50%, #76d880 70%, #ebf38b 90%, #adfbda 100%)",
+    backgroundSize: "100%",
+    backgroundRepeat: "repeat",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+    MozBackgroundClip: "text",
+    filter: "drop-shadow(0 0 2rem #000)",
+  };
   return (
     <>
-      <section className="banner-section container mx-auto py-[100px] flex items-center justify-between">
+      <section className="banner-section container mx-auto py-[80px] flex items-center justify-between">
         <div className="banner-content max-w-xl">
-          <h1 className="text-columbia-blue font-extrabold text-8xl  font-[Gruppo] mb-6">
+          <h1
+            className="font-extrabold text-8xl  font-[Gruppo] mb-6 text-clip"
+            style={rainbowStyle}
+          >
             Amrit Pokharel
           </h1>
+          <p className="text-columbia-blue font-semibold text-[25px] mb-6 ">
+            Hi, I am a Frontend Developer.
+          </p>
           <p className="text-columbia-blue mb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
             repellendus quod ipsa laudantium.
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,17 @@ module.exports = {
         skobeloff: "#116D6E",
         "blue-sapphire": "#116A7B",
         "dark-vanilla": "#CDC2AE",
-        "dirty-white": "#ECE5C7",
+        "dirty-white": "#ECE5C7",s
         "columbia-blue": "#C2DEDC",
         "blue-yonder": "#4F709C",
         temptress: "#321E1E",
       },
+      keyframes: {
+        blink: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
     },
   },
   plugins: [],
