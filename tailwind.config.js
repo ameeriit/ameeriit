551454010d50bc4932375782c2b1535d5ec825a9
@meeriit <37906507+ameeriit@users.noreply.github.com>
2023-06-15 14:02:45 +0545

    changes to banner (#1)
    major changes in banner, adding bg, changed fonts, changed image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
+        jura: ["Jura", "sans-serif"],
+        itim: ["Itim", "sans-serif"],
+        brunoace: ["BrunoAce", "sans-serif"],
       },
       colors: {
         skobeloff: "#116D6E",
@@ -16,28 +18,30 @@ module.exports = {
         "cetacean-blue": "#0E0F34",
         "picton-blue": "#45B1E8 ",
         malachite: "#5FDB86",
+        iris: "#6147C6",
+        "smoky-black": "#0C0D0F",
       },
-      keyframes: {
-        blink: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
-        },
-        typing: {
-          "100%": {
-            left: "100%",
-            margin: "0 15px",
-          },
-        },
-        slide: {
-          "100%": {
-            top: "-80px",
-          },
-        },
-      },
-      animation: {
-        ani: "typing 1.5s steps(9) infinite",
-        sli: "slide 3s steps(2) infinite",
-      },
+      // keyframes: {
+      //   blink: {
+      //     "0%, 100%": { transform: "rotate(-3deg)" },
+      //     "50%": { transform: "rotate(3deg)" },
+      //   },
+      //   typing: {
+      //     "100%": {
+      //       left: "100%",
+      //       margin: "0 15px",
+      //     },
+      //   },
+      //   slide: {
+      //     "100%": {
+      //       top: "-80px",
+      //     },
+      //   },
+      // },
+      // animation: {
+      //   ani: "typing 1.5s steps(9) infinite",
+      //   sli: "slide 3s steps(2) infinite",
+      // },
     },
   },
   plugins: [],
