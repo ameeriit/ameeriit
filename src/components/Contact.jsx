a605b8b637245ba3a6fcb045aacca34862403cb6
@meeriit <amritpokharel51@gmail.com>
2023-06-18 12:19:31 +0545

    contact section update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
+import map from "../assets/images/map.jpg";
+
 const Contact = () => {
   return (
     <section
       id="section2"
-      className="contact-section pt-[40px] pb-[60px] px-[50px]"
+      className="contact-section pt-[40px] pb-[60px] px-[50px] text-smoky-black"
     >
       <div className="contact-wrapper container mx-auto">
         <h2
@@ -11,6 +13,63 @@ const Contact = () => {
         >
           CONTACT
         </h2>
+        <div className="flex gap-[50px]">
+          <form className="flex flex-col w-[50%]">
+            <label
+              className="font-brunoace font-semibold mb-1"
+              htmlFor="fullname"
+            >
+              Full Name:
+            </label>
+            <input
+              className="border-[1px] border-smoky-black px-4 py-2 rounded-lg mb-4 outline-none font-itim font-normal "
+              id="fullname"
+              type="text"
+              placeholder="Full Name..."
+              required
+            />
+            <label className="font-brunoace font-semibold mb-1" htmlFor="email">
+              E-mail:
+            </label>
+            <input
+              className="border-[1px] border-smoky-black px-4 py-2 rounded-lg mb-4 outline-none font-itim font-normal"
+              id="email"
+              type="text"
+              placeholder="E-mail..."
+              required
+            />
+            <label
+              className="font-brunoace font-semibold mb-1"
+              htmlFor="phonenumber"
+            >
+              Phone Number:
+            </label>
+            <input
+              className="border-[1px] border-smoky-black px-4 py-2 rounded-lg mb-4 outline-none font-itim font-normal"
+              id="phonenumber"
+              type="text"
+              placeholder="Phone Number..."
+            />
+            <label
+              className="font-brunoace font-semibold mb-1"
+              htmlFor="message"
+            >
+              Message:
+            </label>
+            <textarea
+              className="border-[1px] border-smoky-black px-4 py-2 rounded-lg mb-4 outline-none font-itim font-normal"
+              id="message"
+              name=""
+              cols="30"
+              rows="10"
+              placeholder="Message..."
+              required
+            ></textarea>
+          </form>
+          <div className="flex justify-center items-center w-[50%]">
+            <img className="rounded-xl" width={500} src={map} alt="" />
+          </div>
+        </div>
       </div>
     </section>
   );
