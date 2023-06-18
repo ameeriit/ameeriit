294402812cf4df0055ddee679094ae0c67cf2982
@meeriit <amritpokharel51@gmail.com>
2023-06-18 16:33:44 +0545

    footer color change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className=" py-4 bg-blue-yonder">
+      <footer className=" py-4 bg-smoky-black">
         <div className="footer-wrapper container mx-auto text-center">
           <p className="text-columbia-blue text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] px-2 sm:px-4">
             Copyright &copy; Amrit Pokharel. All Rights Reserved.
