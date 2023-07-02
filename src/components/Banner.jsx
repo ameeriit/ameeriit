43e54024b4bcb06687a7ac46c74fc18e4b745326
@meeriit <37906507+ameeriit@users.noreply.github.com>
2023-07-02 22:35:50 +0545

    cv zz

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -84,7 +84,7 @@ const Banner = () => {
             <br />
 
             <a
-              href="/amritpokharel-cv.pdf"
+              href="//amritpokharel-cv.pdf"
               className="relative inline-block px-4 py-2 font-medium group"
             >
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-green-cm border-2 border-green-cm group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
