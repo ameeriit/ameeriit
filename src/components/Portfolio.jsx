680ab9fa2fbad96d770df4db97dfd39a19bd5c84
@meeriit <37906507+ameeriit@users.noreply.github.com>
2023-07-02 22:40:29 +0545

    project added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,6 +18,18 @@ const Portfolio = () => {
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat error, architecto qui in, laudantium ullam veritatis cupiditate, nostrum sit officia officiis assumenda ex libero repudiandae? Vel temporibus facere corporis natus!",
       languages: ["HTML", "CSS", "Node.js", "Express"],
     },
+    {
+      name: "Movie App",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat error, architecto qui in, laudantium ullam veritatis cupiditate, nostrum sit officia officiis assumenda ex libero repudiandae? Vel temporibus facere corporis natus!",
+      languages: ["HTML", "CSS", "React", "Restful-Api"],
+    },
+    {
+      name: "Landing Pages",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat error, architecto qui in, laudantium ullam veritatis cupiditate, nostrum sit officia officiis assumenda ex libero repudiandae? Vel temporibus facere corporis natus!",
+      languages: ["HTML", "CSS", "Jquery", "WordPress"],
+    },
   ];
 
   return (
